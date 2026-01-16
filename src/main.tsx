442102e8f7f946a9af2afe16300f872f28a63c86
sumit9722 <shahsumit9722@gmail.com>
2026-01-17 04:20:14 +0530

    registration update --will not work on netlify toh try merge mat karna please bsdk

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { CookiesProvider } from 'react-cookie'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <CookiesProvider>
+            <GoogleOAuthProvider clientId="513838793862-gfa9g1snl74coi736ggti5nd9uglh66k.apps.googleusercontent.com">
+                <App />
+            </GoogleOAuthProvider>
+        </CookiesProvider>
+    </StrictMode>,
 )
