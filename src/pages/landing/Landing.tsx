56393736426eb4ac8d4f07d538756f55c4f235a5
AnonymousTechDeveloper <anshmalviya54@gmail.com>
2026-01-21 23:20:21 +0530

    Added: Car (Infernus) and Orbital controls

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,101 +1,112 @@
 import styles from "./Landing.module.scss";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { Html, ScrollControls, Text, useScroll } from "@react-three/drei";
+import { Html, ScrollControls, Text, useGLTF, useScroll ,OrbitControls} from "@react-three/drei";
 import { Perf } from "r3f-perf";
 import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 
 /* ---------- Cube ---------- */
 type CubeProps = {
-  cubeRef: React.RefObject<THREE.Mesh>;
+    cubeRef: React.RefObject<THREE.Mesh>;
 };
 
 function Cube({ cubeRef }: CubeProps) {
-  return (
-    <mesh ref={cubeRef} position={[0, 0, 0]}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="orange" />
-      <Text
-        position={[-0.5001, 0, 0]}
-        fontSize={0.25}
-        color="white"
-        anchorX="center"
-        anchorY="middle"
-        rotation={[0, -Math.PI / 2, 0]}
-      >
-        APOGEE
-      </Text>
-    </mesh>
-  );
+    return (
+        <mesh ref={cubeRef} position={[0, 0, 0]}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="orange" />
+            <Text
+                position={[-0.5001, 0, 0]}
+                fontSize={0.25}
+                color="white"
+                anchorX="center"
+                anchorY="middle"
+                rotation={[0, -Math.PI / 2, 0]}
+            >
+                APOGEE
+            </Text>
+        </mesh>
+    );
+}
+
+function Infernus(props: any) {
+    const {scene} = useGLTF("./src/assets/3d/landing/car3.glb");
+    return (
+        <group {...props}>
+            <primitive object={scene}  />
+        </group>
+    )
 }
 
 /* ---------- Camera Controller ---------- */
 type CameraControllerProps = {
-  cubeRef: React.RefObject<THREE.Mesh>;
-  setPage: (p: number) => void;
+    infernusRef: React.RefObject<THREE.Group>;
+    setPage: (p: number) => void;
 };
 
-function CameraController({ cubeRef, setPage }: CameraControllerProps) {
-  const { camera } = useThree();
-  const scroll = useScroll();
+function CameraController({ infernusRef, setPage }: CameraControllerProps) {
+    const { camera } = useThree();
+    const scroll = useScroll();
 
-  useFrame(() => {
-    const pageProgress = THREE.MathUtils.clamp(scroll.offset * 4, 0, 1);
+    useFrame(() => {
+        const pageProgress = THREE.MathUtils.clamp(scroll.offset * 4, 0, 1);
 
-    const scaleAmount = 0.5;
-    const scale = 1 + pageProgress * scaleAmount;
+        const scaleAmount = 0.005;
+        const scale = 0.01 + pageProgress * scaleAmount;
 
-    if (cubeRef.current) {
-      cubeRef.current.rotation.y = pageProgress * Math.PI / 2;
-      cubeRef.current.scale.setScalar(scale);
-      camera.lookAt(cubeRef.current.position);
-    }
+        if (infernusRef.current) {
+            infernusRef.current.rotation.y = pageProgress * Math.PI / 2;
+            infernusRef.current.scale.setScalar(scale);
+            camera.lookAt(infernusRef.current.position);
+        }
 
-    // update current page index
-    const pageIndex = Math.floor(scroll.offset * 4);
-    setPage(pageIndex);
-  });
+        // update current page index
+        const pageIndex = Math.floor(scroll.offset * 4);
+        setPage(pageIndex);
+    });
 
-  return null;
+    return null;
 }
 
 function Scene({ setPage }: { setPage: (p: number) => void }) {
-  const cubeRef = useRef<THREE.Mesh>(null!);
+    const cubeRef = useRef<THREE.Mesh>(null!);
+    const infernusRef = useRef<THREE.Group>(null!)
 
-  return (
-    <>
-      <ambientLight intensity={0.9} />
-      <Cube cubeRef={cubeRef} />
-      <CameraController cubeRef={cubeRef} setPage={setPage} />
-    </>
-  );
-}
+    return (
+        <>
+            <ambientLight intensity={0.9} />
+            <Infernus ref={infernusRef} />
+            <CameraController infernusRef={infernusRef} setPage={setPage} />
+        </>
+    );
+}``
 
 export default function Landing() {
-const pages = [
-  { id: 0, url: "/" },
-  { id: 1, url: "/" },
-  { id: 2, url: "/events" },
-  { id: 3, url: "/aboutus" },
-];
+    const pages = [
+        { id: 0, url: "/" },
+        { id: 1, url: "/" },
+        { id: 2, url: "/events" },
+        { id: 3, url: "/aboutus" },
+    ];
 
-const [page, setPage] = useState(0);
+    const [page, setPage] = useState(0);
 
-useEffect(() => {
-  const url = pages[page]?.url || "/";
-  history.replaceState(null, "", url);
-}, [page]);
+    useEffect(() => {
+        const url = pages[page]?.url || "/";
+        history.replaceState(null, "", url);
+    }, [page]);
 
 
 
-  return (
-    <>
-      <Canvas camera={{ position: [0, 0, 5] }}>
-        <Perf />
-        <ScrollControls pages={4} damping={0.2}>
-          <Scene setPage={setPage} />
-        </ScrollControls>
-      </Canvas>
-    </>
-  );
+    return (
+        <>
+            <Canvas camera={{ position: [0, 0, 5] }}>
+                <OrbitControls/>
+                <Perf />
+                <ScrollControls pages={4} damping={0.2}>
+                    <Scene setPage={setPage} />
+                </ScrollControls>
+            </Canvas>
+        </>
+    );
 }
