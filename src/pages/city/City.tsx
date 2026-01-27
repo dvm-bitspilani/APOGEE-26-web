aa0ef57806c9caf0357dcb073069805ecdce1690
Atharv Agarwal <atharv20agarwal@gmail.com>
2026-01-27 23:37:25 +0530

    feat: Display a 3D city model on the City page using React Three Fiber and a new GLB asset.

--- src/pages/city/City.tsx
+++ src/pages/city/City.tsx
@@ -1,7 +1,21 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Perf } from "r3f-perf";
+import { Instances, Model } from "./Model";
+
 export default function City() {
     return (
-        <div>
-            <h1>City</h1>
-        </div>
+        <Canvas
+                camera={{ position: [0, 0, 5] }}
+                style={{ width: "100%", height: "100%" }}
+              >
+                <OrbitControls/>
+
+                {import.meta.env.DEV && <Perf position="top-left" />}
+                <ambientLight intensity={0.9} />
+                <Instances>
+                  <Model />
+                </Instances>
+              </Canvas>
     )
 }
