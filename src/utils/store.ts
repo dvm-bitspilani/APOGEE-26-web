ca8f26853bcba6b0d708f7cec0e935720eea9a3f
Ansh <76505499+AnonymousTechDeveloper@users.noreply.github.com>
2026-01-29 00:07:44 +0530

    Update: Set default reg state to Instructions

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -39,7 +39,7 @@ interface RegistrationStore {
 }
 
 export const useRegistrationStore = create<RegistrationStore>((set) => ({
-  registrationStep: 'details',
+  registrationStep: 'instructions',
   setRegistrationStep: (step) => set({ registrationStep: step }),
 
   events: [],
