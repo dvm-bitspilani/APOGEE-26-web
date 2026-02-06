722ba81899c335854ae1d3d91a12bd2539f94c33
Atharv Agarwal <atharv20agarwal@gmail.com>
2026-02-06 21:49:36 +0530

    frames update

--- src/pages/videoLanding/videoLandingScrolly.tsx
+++ src/pages/videoLanding/videoLandingScrolly.tsx
@@ -3,7 +3,7 @@ import ScrollyVideo from 'scrolly-video/dist/ScrollyVideo.esm.jsx';
 export default function VideoLandingScrolly() {
     return (
         <div style={{height: '500vh'}}>
-            <ScrollyVideo src="/videos/landingBg.mp4" />
+            <ScrollyVideo src="/videos/landingBg.mp4" transitionSpeed={1} />
         </div>
     );
 }
