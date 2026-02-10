cb7a7c038cd14c9854e193dee2cb9e988c073d6f
sumit9722 <shahsumit9722@gmail.com>
2026-02-10 19:03:04 +0530

    nav bar commented out

--- src/pages/city/City.tsx
+++ src/pages/city/City.tsx
@@ -13,7 +13,7 @@ import { useEffect, useState } from "react";
 // import debugFunctions from "../../utils/debug";
 import * as THREE from "three";
 import { useModalStore, useSceneLoadedStore } from "../../utils/store";
-import NavBar from "../components/NavBar/NavBar";
+// import NavBar from "../components/NavBar/NavBar";
 import RegisterButton from "../components/RegisterButton/RegisterButton";
 import Preloader from "../preloader/Preloader";
 import Modal from "./components/Modal/Modal";
@@ -125,7 +125,7 @@ export default function City() {
           {/* </Html> */}
         </div>
       }
-      <NavBar />
+      {/* <NavBar /> */}
       <RegisterButton />
       {isModalopen && <Modal />}
     </>
