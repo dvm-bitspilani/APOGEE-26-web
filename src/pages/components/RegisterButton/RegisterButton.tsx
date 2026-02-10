a72076abb0806923ae010561aab4cb9e2951c63a
sumit9722 <shahsumit9722@gmail.com>
2026-02-10 21:21:10 +0530

    Reg bol

--- src/pages/components/RegisterButton/RegisterButton.tsx
+++ src/pages/components/RegisterButton/RegisterButton.tsx
@@ -1,24 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./RegisterButton.module.scss";
-import { useState, useEffect } from "react";
-import NavButton from "../../registration/components/navButton/NavButton";
 
 export default function RegisterButton() {
     const navigate = useNavigate();
-    const [isMobile, setIsMobile] = useState(false);
 
-    useEffect(() => {
-        const checkMobile = () => {
-            setIsMobile(window.innerWidth <= 768);
-        };
-        checkMobile();
-        window.addEventListener("resize", checkMobile);
-        return () => window.removeEventListener("resize", checkMobile);
-    }, []);
-
-    return isMobile ? (
-        <NavButton onClick={() => navigate("/registration")} outerClass={styles.confirmButton} innerClass={styles.confirmButtonContent}>Register</NavButton>
-    ) : (
+    return (
         <div className={styles.registerButton} onClick={() => navigate("/registration")}>
             <img
                 className={styles.registerIcon}
