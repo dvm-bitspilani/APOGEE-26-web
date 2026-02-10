1c2aab8753a2dd0b752644d1cbe42facafbc2398
sumit9722 <shahsumit9722@gmail.com>
2026-02-10 19:33:52 +0530

    landing register button mobile change

--- src/pages/components/RegisterButton/RegisterButton.tsx
+++ src/pages/components/RegisterButton/RegisterButton.tsx
@@ -1,13 +1,31 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./RegisterButton.module.scss";
+import { useState, useEffect } from "react";
+import NavButton from "../../registration/components/navButton/NavButton";
 
 export default function RegisterButton() {
     const navigate = useNavigate();
+    const [isMobile, setIsMobile] = useState(false);
 
-    return (
+    useEffect(() => {
+        const checkMobile = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+        checkMobile();
+        window.addEventListener("resize", checkMobile);
+        return () => window.removeEventListener("resize", checkMobile);
+    }, []);
+
+    return isMobile ? (
+        <NavButton onClick={() => navigate("/registration")} outerClass={styles.confirmButton} innerClass={styles.confirmButtonContent}>Register</NavButton>
+    ) : (
         <div className={styles.registerButton} onClick={() => navigate("/registration")}>
-            <img className={styles.registerIcon} src="/img/landing/reg_portal.png" alt="Register Image" />
+            <img
+                className={styles.registerIcon}
+                src="/img/landing/reg_portal.png"
+                alt="Register Image"
+            />
             <div className={styles.registerText}>Register</div>
         </div>
-    )
+    );
 }
