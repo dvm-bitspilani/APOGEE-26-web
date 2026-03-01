d4c08dbf2f17074bddb3dca55cb98e3e140b81f6
varprabhlite <185780179+varprabhlite@users.noreply.github.com>
2026-03-01 16:04:47 +0530

    fix responsiveness of about us

--- src/pages/aboutUs/AboutUs.tsx
+++ src/pages/aboutUs/AboutUs.tsx
@@ -45,15 +45,6 @@ export default function AboutUs() {
 
         <div className={styles.rightColumn}>
           <div className={styles.videoContainer}>
-            <img
-              src="/svg/aboutUs/arrow.svg"
-              className={`${styles.arrow} ${styles.leftArrow}`}
-              alt="Previous"
-              onClick={(e) => {
-                e.stopPropagation();
-                prevVideo();
-              }}
-            />
             <div className={styles.videoPlaceholder}>
               {/* Render the container for YouTube Player */}
               <div
@@ -78,15 +69,27 @@ export default function AboutUs() {
                 ></div>
               )}
             </div>
-            <img
-              src="/svg/aboutUs/arrow.svg"
-              className={`${styles.arrow} ${styles.rightArrow}`}
-              alt="Next"
-              onClick={(e) => {
-                e.stopPropagation();
-                nextVideo();
-              }}
-            />
+
+            <div className={styles.arrowsContainer}>
+              <img
+                src="/svg/aboutUs/arrow.svg"
+                className={`${styles.arrow} ${styles.leftArrow}`}
+                alt="Previous"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  prevVideo();
+                }}
+              />
+              <img
+                src="/svg/aboutUs/arrow.svg"
+                className={`${styles.arrow} ${styles.rightArrow}`}
+                alt="Next"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  nextVideo();
+                }}
+              />
+            </div>
           </div>
 
           <div className={styles.socials} onClick={(e) => e.stopPropagation()}>
