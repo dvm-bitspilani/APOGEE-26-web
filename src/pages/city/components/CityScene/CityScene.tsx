3bbebf46e1e5aa94dc2f6879d136034990a3a0e0
Ishaan02-Coder <ishaandaftari2@gmail.com>
2026-03-01 12:38:20 +0530

    Unable to solve returning from ham issue

--- src/pages/city/components/CityScene/CityScene.tsx
+++ src/pages/city/components/CityScene/CityScene.tsx
@@ -174,10 +174,10 @@ useFrame(() => {
       theatreCamera.quaternion.angleTo(normalCamQuat.current) < 0.001
    if(!sheet ) return
     if (posDone && rotDone) {
-      // theatreCamera.position.copy(normalCamPos.current)
-      // theatreCamera.quaternion.copy(normalCamQuat.current)
+      theatreCamera.position.copy(normalCamPos.current)
+      theatreCamera.quaternion.copy(normalCamQuat.current)
 sheet.sequence.position = savedSequencePos.current
-sheet.sequence.play({ iterationCount:1 })
+sheet.sequence.play({ iterationCount:Infinity })
 setReturning(false)
     }
   }
