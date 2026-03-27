051000f017a6f8414a1fe751c07c58d08b3f819e
AnonymousTechDeveloper <anshmalviya54@gmail.com>
2026-03-27 14:01:26 +0530

    Changed modal trigger range to exclude extremes

--- src/pages/city/components/ScrollSync/ScrollSync.tsx
+++ src/pages/city/components/ScrollSync/ScrollSync.tsx
@@ -84,8 +84,8 @@ export default function ScrollSync() {
       for (const path in stopPoints) {
         if (
           path !== currentSection &&
-          scrollSheet.sequence.position >= stopPoints[path as Section]?.[0] &&
-          scrollSheet.sequence.position <= stopPoints[path as Section]?.[1]
+          scrollSheet.sequence.position > stopPoints[path as Section]?.[0] &&
+          scrollSheet.sequence.position < stopPoints[path as Section]?.[1]
         ) {
           if (path !== "home") {
             openModal();
@@ -96,8 +96,8 @@ export default function ScrollSync() {
       }
     } else {
       if (
-        scrollSheet.sequence.position <= stopPoints[currentSection]?.[0] ||
-        scrollSheet.sequence.position >= stopPoints[currentSection]?.[1]
+        scrollSheet.sequence.position < stopPoints[currentSection]?.[0] ||
+        scrollSheet.sequence.position > stopPoints[currentSection]?.[1]
       ) {
         closeModal();
         setTimeout(() => setCurrentSection("transition"), 500)
