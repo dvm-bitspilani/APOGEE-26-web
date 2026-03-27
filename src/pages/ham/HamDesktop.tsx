5f2066306415fb2da32f1578eb30578ccdf8a632
Pranjal Gupta <pranjalgupta58.97@gmail.com>
2026-03-27 12:38:58 +0530

    ADD: ham mobile menu

---
+++ src/pages/ham/HamDesktop.tsx
@@ -0,0 +1,406 @@
+import { useEffect, useMemo, useRef, useState } from "react";
+import styles from "./HamDesktop.module.scss";
+import bg from "/img/ham/bg.png";
+import dott from "/img/ham/dott.png";
+import textBottom from "/img/ham/textBottom.png";
+import luv from "/img/ham/luv_dvm.png";
+import gsap from "gsap";
+
+export default function Ham() {
+  const [speedText, setspeedText] = useState(0);
+  const [totalPurple, settotalPurple] = useState(0);
+  const [totalSpeedPurple, settotalSpeedPurple] = useState(6);
+  const [startHovering, setstartHovering] = useState(false);
+  const hoverTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const speedDashTicksRef = useRef<HTMLDivElement[]>([]);
+  const bigDashTicksRef = useRef<HTMLDivElement[]>([]);
+  const nameTagRef = useRef<HTMLDivElement[]>([]);
+  const radialCircle1Ref = useRef<HTMLDivElement>(null);
+  const radialCircle2Ref = useRef<HTMLDivElement>(null);
+  const speedDialRef = useRef<HTMLDivElement>(null);
+  const mainSpeedometerRef = useRef<HTMLDivElement>(null);
+  const luvRef = useRef<HTMLDivElement>(null);
+
+  const onUpdate = (duration: number, speedTicks?: number) => {
+    const rot = gsap.getProperty(
+      radialCircle1Ref.current,
+      "rotation",
+    ) as number;
+    setspeedText(Math.round(((rot + 180) / 180) * 400));
+    if (speedTicks) {
+      bigDashTicksUpdate(duration, 0.5);
+      speedDashTicksUpdate(speedTicks, 0.5);
+    } else {
+      speedDashTicksUpdate(Math.round(((rot + 180) / 180) * 6), duration);
+      bigDashTicksUpdate(Math.round(((rot + 180) / 180) * 10), duration);
+      nameTagUpdate(Math.round(((rot + 180) / 180) * 6));
+    }
+  };
+  const nameTagUpdate = (id: number) => {
+    nameTagRef.current.forEach((tag, i) => {
+      if (i === id) {
+        gsap.to(tag, {
+          scale: 1,
+          x: 0,
+          duration: 0.2,
+          ease: "power1.inOut",
+        });
+      }
+    });
+  };
+
+  const bigDashTicksUpdate = (toPurple: number, duration: number) => {
+    const tickTL = gsap.timeline();
+    const totalTicks = bigDashTicksRef.current.length;
+    Array.from({ length: totalTicks }).forEach((_, i) => {
+      const currentTick = totalPurple > toPurple ? totalTicks - i - 1 : i;
+      tickTL.to(bigDashTicksRef.current[currentTick], {
+        // duration: totalPurple > toPurple ? 0.5 / totalTicks : 0.5 / toPurple,
+        duration: duration / totalTicks,
+        opacity: () => (currentTick < toPurple ? 1 : 0),
+      });
+    });
+    settotalPurple(toPurple);
+  };
+  const speedDashTicksUpdate = (toPurple: number, duration: number) => {
+    const tickTL2 = gsap.timeline();
+    const totalTicks = speedDashTicksRef.current.length;
+    Array.from({ length: totalTicks }).forEach((_, i) => {
+      const currentTick = totalSpeedPurple > toPurple ? totalTicks - i - 1 : i;
+      tickTL2.to(speedDashTicksRef.current[currentTick], {
+        // duration: totalPurple > toPurple ? 0.5 / totalTicks : 0.5 / toPurple,
+        duration: duration / totalTicks,
+        backgroundColor: () => (currentTick < toPurple ? "#b300ff" : "#4df4fc"),
+        onStart: () => {
+          speedDashTicksRef.current[currentTick].style.setProperty(
+            `--afterBG${currentTick + 1}`,
+            currentTick < toPurple ? "#b300ff" : "#4df4fc",
+          );
+        },
+      });
+    });
+    settotalSpeedPurple(toPurple);
+  };
+
+  const nameTags = useMemo(() => {
+    return [
+      {
+        id: 1,
+        animId: 3,
+        label: "<\u00A0 developers",
+        side: "Left",
+        mouseEnter: () => {
+          gsap.to(speedDialRef.current, {
+            rotation: -36,
+            duration: 0.5,
+            onUpdate: () => onUpdate(3, 3),
+          });
+          gsap.to(radialCircle1Ref.current, { rotation: -127, duration: 0.5 });
+          gsap.to(radialCircle2Ref.current, { rotation: 55, duration: 0.5 });
+          // bigDashTicksUpdate(3);
+          // speedDashTicksUpdate(3);
+        },
+      },
+      {
+        id: 2,
+        animId: 2,
+        label: "<\u00A0 contact us",
+        side: "Left",
+        mouseEnter: () => {
+          gsap.to(speedDialRef.current, {
+            rotation: -54.5,
+            duration: 0.5,
+            onUpdate: () => onUpdate(2, 2),
+          });
+          gsap.to(radialCircle1Ref.current, {
+            rotation: -145.5,
+            duration: 0.5,
+          });
+          gsap.to(radialCircle2Ref.current, { rotation: 36.5, duration: 0.5 });
+          // bigDashTicksUpdate(2);
+          // speedDashTicksUpdate(2);
+        },
+      },
+      {
+        id: 3,
+        animId: 1,
+        label: "<\u00A0\u00A0 state",
+        side: "Left",
+        mouseEnter: () => {
+          gsap.to(speedDialRef.current, {
+            rotation: -73,
+            duration: 0.5,
+            onUpdate: () => onUpdate(1, 1),
+          });
+          gsap.to(radialCircle1Ref.current, { rotation: -164, duration: 0.5 });
+          gsap.to(radialCircle2Ref.current, { rotation: 18, duration: 0.5 });
+          // bigDashTicksUpdate(1);
+          // speedDashTicksUpdate(1);
+        },
+      },
+      {
+        id: 4,
+        animId: 4,
+        label: "media partners >",
+        side: "Right",
+        mouseEnter: () => {
+          gsap.to(speedDialRef.current, {
+            rotation: 36,
+            duration: 0.5,
+            onUpdate: () => onUpdate(7, 4),
+          });
+          gsap.to(radialCircle1Ref.current, { rotation: -55, duration: 0.5 });
+          gsap.to(radialCircle2Ref.current, { rotation: 126, duration: 0.5 });
+          // bigDashTicksUpdate(7);
+          // speedDashTicksUpdate(4);
+        },
+      },
+      {
+        id: 5,
+        animId: 5,
+        label: "sponsors\u00A0\u00A0 >",
+        side: "Right",
+        mouseEnter: () => {
+          gsap.to(speedDialRef.current, {
+            rotation: 55,
+            duration: 0.5,
+            onUpdate: () => onUpdate(8, 5),
+          });
+          gsap.to(radialCircle1Ref.current, { rotation: -36, duration: 0.5 });
+          gsap.to(radialCircle2Ref.current, { rotation: 145, duration: 0.5 });
+          // bigDashTicksUpdate(8);
+          // speedDashTicksUpdate(5);
+        },
+      },
+      {
+        id: 6,
+        animId: 6,
+        label: "events\u00A0 >",
+        side: "Right",
+        mouseEnter: () => {
+          gsap.to(speedDialRef.current, {
+            rotation: 73,
+            duration: 0.5,
+            onUpdate: () => onUpdate(9, 6),
+          });
+          gsap.to(radialCircle1Ref.current, { rotation: -18, duration: 0.5 });
+          gsap.to(radialCircle2Ref.current, { rotation: 161, duration: 0.5 });
+          // bigDashTicksUpdate(10);
+          // speedDashTicksUpdate(6);
+        },
+      },
+    ];
+  }, []);
+
+  // Function to display speed dash ticks based on mouse position
+  // const displaySpeedDashTicks = (x: number, y: number) => {
+  //   const theta = Math.atan2(y, x);
+  //   const angle = (theta * 180) / Math.PI;
+  //   const index = Math.round(
+  //     ((180 - angle) / 180) * speedDashTicksRef.current.length,
+  //   );
+  //   speedDashTicksRef.current.forEach((tick, i) => {
+  //     if (i > index) {
+  //       tick.style.display = "none";
+  //     } else {
+  //       tick.style.display = "block";
+  //     }
+  //   });
+  // };
+
+  // useEffect(() => {
+  //   displaySpeedDashTicks(0.72, 1.68);
+
+  //   const onMove = (e: MouseEvent) => {
+  //     displaySpeedDashTicks(
+  //       (e.clientX / window.innerWidth) * 2 - 1,
+  //       2 - (e.clientY / window.innerHeight) * 2,
+  //     );
+  //   };
+
+  //   window.addEventListener("mousemove", onMove);
+  //   return () => window.removeEventListener("mousemove", onMove);
+  // }, []);
+
+  useEffect(() => {
+    const startAnim = () => {
+      const mainDuration = 0.6;
+      gsap
+        .timeline({
+          defaults: { ease: "power1.inOut" },
+          onUpdate: () => onUpdate(0.02),
+          onComplete: () => {
+            setstartHovering(true);
+          },
+        })
+        .fromTo(
+          radialCircle1Ref.current,
+          { xPercent: -50, yPercent: -50, rotation: -180 },
+          { rotation: -70, duration: mainDuration },
+        )
+        .to(radialCircle1Ref.current, {
+          rotation: -120,
+          duration: mainDuration,
+        })
+        .to(radialCircle1Ref.current, { rotation: 0, duration: mainDuration });
+
+      gsap
+        .timeline({ defaults: { ease: "power1.inOut" } })
+        .fromTo(
+          speedDialRef.current,
+          { xPercent: -50, yPercent: -100, rotation: -90 },
+          { rotation: 20, duration: mainDuration },
+        )
+        .to(speedDialRef.current, { rotation: -30, duration: mainDuration })
+        .to(speedDialRef.current, { rotation: 90, duration: mainDuration });
+
+      gsap
+        .timeline({ defaults: { ease: "power1.inOut" } })
+        .fromTo(
+          radialCircle2Ref.current,
+          { xPercent: -50, yPercent: -50, rotation: 0 },
+          { rotation: 110, duration: mainDuration },
+        )
+        .to(radialCircle2Ref.current, { rotation: 60, duration: mainDuration })
+        .to(radialCircle2Ref.current, {
+          rotation: 180,
+          duration: mainDuration,
+        });
+    };
+    gsap
+      .timeline({ defaults: { ease: "power1.inOut" }, onComplete: startAnim })
+      .fromTo(
+        mainSpeedometerRef.current,
+        { scale: 0, opacity: 0 },
+        { scale: 1, opacity: 1, duration: 0.3 },
+      )
+      .fromTo(
+        luvRef.current,
+        { scale: 0, opacity: 0, y: -20 },
+        { scale: 1, opacity: 1, y: 0, duration: 0.2 },
+      );
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.scanlineoverlay}></div>
+      <img src={bg} alt="Ham" className={styles.bgImg} />
+      <div className={styles.speedometer} ref={mainSpeedometerRef}>
+        {/* Inner dash dash lines jo multiple hai */}
+        {Array.from({ length: 7 }).map((_, i) => (
+          <div key={i} className={`${styles[`dashCircle${i + 1}`]}`}></div>
+        ))}
+
+        {/* Speedometer speed */}
+        <div className={styles.speedFont}>
+          <span>{speedText}</span>
+        </div>
+
+        {/* Actual speedometer coloured bar */}
+        <div className={styles.radialCircle1} ref={radialCircle1Ref}></div>
+
+        {/* Actual speedometer transparent bar */}
+        <div className={styles.radialCircle2} ref={radialCircle2Ref}></div>
+
+        {/* Out of speedometer bar colour wala */}
+        {/* <div className={styles.bigDashCircle}></div> */}
+        {Array.from({ length: 10 }).map((_, i) => (
+          <div
+            key={i}
+            className={`${styles[`bigDashTicks${i + 1}`]}`}
+            ref={(el) => {
+              if (el) bigDashTicksRef.current[i] = el;
+            }}
+          ></div>
+        ))}
+
+        {/* Out of speedometer bar uncolour wala */}
+        <div className={styles.bigDashCircleBehind}></div>
+
+        {/* Speedometer bar pe jo lines hai */}
+        <div className={styles.speedoDashCircle}></div>
+
+        {/* Speedometer bar ke uppar jo ticks hai purple wale */}
+        {Array.from({ length: 6 }).map((_, i) => (
+          <div
+            key={i}
+            className={`${styles[`speedTick${i + 1}`]}`}
+            ref={(el) => {
+              if (el) speedDashTicksRef.current[i] = el;
+            }}
+          ></div>
+        ))}
+
+        {/* Speedometer ke andar ka bhadiya wala design */}
+        <img src={dott} alt="dott" className={styles.dottedIMG} />
+
+        {/* Speed dial */}
+        <div className={styles.speedDial} ref={speedDialRef}>
+          <svg
+            width="12"
+            height="200"
+            viewBox="0 0 12 200"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 0C7.99944 -4.7716e-08 3.99908 0.0543932 0 0.163165L2.82646 200C5.01491 199.94 7.13721 199.911 9.32646 199.911L12 0Z"
+              fill="url(#paint0_radial_615_230)"
+            />
+            <defs>
+              <radialGradient
+                id="paint0_radial_615_230"
+                cx="0"
+                cy="0"
+                r="1"
+                gradientTransform="matrix(135.231 -458.637 410.007 120.942 30.6524 441.535)"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop offset="0.389672" stopColor="#EEE100" />
+                <stop offset="0.950824" stopColor="#B301FF" />
+              </radialGradient>
+            </defs>
+          </svg>
+        </div>
+
+        {/* Name tags saare */}
+        {nameTags.map(({ id, label, side, mouseEnter, animId }) => (
+          <div
+            ref={(el) => {
+              if (el) nameTagRef.current[animId] = el;
+            }}
+            key={id}
+            className={styles[`nameTagCont${id}`]}
+            onMouseEnter={() => {
+              if (mouseEnter && startHovering) {
+                hoverTimeoutRef.current = setTimeout(() => {
+                  mouseEnter();
+                }, 200);
+              }
+            }}
+            onMouseLeave={() => {
+              if (hoverTimeoutRef.current && startHovering) {
+                clearTimeout(hoverTimeoutRef.current);
+                hoverTimeoutRef.current = null;
+              }
+            }}
+          >
+            <div className={styles[`nameTag${side}${id}`]}>
+              <span>{label}</span>
+              <img
+                src={textBottom}
+                alt="textBottom"
+                className={styles.textBottomIMG}
+              />
+            </div>
+            <div className={styles[`nameTagBG${id}`]}></div>
+            <div className={styles[`nameTagBorder${side}${id}`]}></div>
+          </div>
+        ))}
+      </div>
+      <div className={styles.luvBar} ref={luvRef}>
+        Made with <img src={luv} alt="love" /> by DVM
+      </div>
+    </div>
+  );
+}
--- src/pages/ham/HamMobile.tsx
+++ src/pages/ham/HamMobile.tsx
@@ -1,41 +1,42 @@
-// import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./HamMobile.module.scss";
 import bg from "/img/ham/bg.png";
 import luv from "/img/ham/luv_dvm.png";
-// import gsap from "gsap";
+import textBottom from "/img/ham/textBottom.png";
+import gsap from "gsap";
 
 function SvgEl() {
   return (
     <svg
-      width="334"
-      height="61"
-      viewBox="0 0 334 61"
+      width="333"
+      height="72"
+      viewBox="0 0 333 72"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <mask
-        id="path-1-outside-1_1762_2056"
+        id="path-1-outside-1_649_278"
         maskUnits="userSpaceOnUse"
-        x="-0.393555"
+        x="-0.507812"
         y="0"
         width="334"
-        height="61"
+        height="72"
         fill="black"
       >
-        <rect fill="white" x="-0.393555" width="334" height="61" />
-        <path d="M331.701 1H30.3814L1.60645 59.5H312.747C317.645 39.4289 324.001 19.8931 331.701 1Z" />
+        <rect fill="white" x="-0.507812" width="334" height="72" />
+        <path d="M331.492 1H30.2589L1.49219 71H312.543C317.44 46.9833 323.794 23.6071 331.492 1Z" />
       </mask>
       <path
-        d="M331.701 1H30.3814L1.60645 59.5H312.747C317.645 39.4289 324.001 19.8931 331.701 1Z"
+        d="M331.492 1H30.2589L1.49219 71H312.543C317.44 46.9833 323.794 23.6071 331.492 1Z"
         fill="black"
-        fill-opacity="0.3"
+        fillOpacity="0.3"
       />
       <g
-        clip-path="url(#paint0_angular_1762_2056_clip_path)"
+        clipPath="url(#paint0_angular_649_278_clip_path)"
         data-figma-skip-parse="true"
-        mask="url(#path-1-outside-1_1762_2056)"
+        mask="url(#path-1-outside-1_649_278)"
       >
-        <g transform="matrix(0 0.240399 -0.47815 0 771.881 -38.3994)">
+        <g transform="matrix(0 0.287657 -0.478013 0 771.546 -46.1446)">
           <foreignObject
             x="-1613.04"
             y="-1613.04"
@@ -56,15 +57,15 @@ function SvgEl() {
         </g>
       </g>
       <path
-        d="M331.701 1L332.627 1.37743L333.189 0H331.701V1ZM30.3814 1V0H29.7589L29.4841 0.558625L30.3814 1ZM1.60645 59.5L0.709123 59.0586L0.00013864 60.5H1.60645V59.5ZM312.747 59.5V60.5H313.532L313.718 59.7371L312.747 59.5ZM331.701 1V0H30.3814V1V2H331.701V1ZM30.3814 1L29.4841 0.558625L0.709123 59.0586L1.60645 59.5L2.50377 59.9414L31.2788 1.44138L30.3814 1ZM312.747 59.5L313.718 59.7371C318.605 39.7144 324.945 20.2255 332.627 1.37743L331.701 1L330.775 0.622573C323.057 19.5607 316.686 39.1434 311.775 59.2629L312.747 59.5ZM1.60645 59.5V60.5H312.747V59.5V58.5H1.60645V59.5Z"
-        data-figma-gradient-fill="{&#34;type&#34;:&#34;GRADIENT_ANGULAR&#34;,&#34;stops&#34;:[{&#34;color&#34;:{&#34;r&#34;:0.20000000298023224,&#34;g&#34;:0.90588235855102539,&#34;b&#34;:1.0,&#34;a&#34;:1.0},&#34;position&#34;:0.0},{&#34;color&#34;:{&#34;r&#34;:0.0,&#34;g&#34;:0.0,&#34;b&#34;:0.0,&#34;a&#34;:1.0},&#34;position&#34;:0.50024455785751343}],&#34;stopsVar&#34;:[{&#34;color&#34;:{&#34;r&#34;:0.20000000298023224,&#34;g&#34;:0.90588235855102539,&#34;b&#34;:1.0,&#34;a&#34;:1.0},&#34;position&#34;:0.0},{&#34;color&#34;:{&#34;r&#34;:0.0,&#34;g&#34;:0.0,&#34;b&#34;:0.0,&#34;a&#34;:1.0},&#34;position&#34;:0.50024455785751343}],&#34;transform&#34;:{&#34;m00&#34;:5.3956063792229281e-13,&#34;m01&#34;:-956.30059814453125,&#34;m02&#34;:1250.031738281250,&#34;m10&#34;:480.7988281250,&#34;m11&#34;:4.3387290830400327e-13,&#34;m12&#34;:-278.79885864257812},&#34;opacity&#34;:1.0,&#34;blendMode&#34;:&#34;NORMAL&#34;,&#34;visible&#34;:true}"
-        mask="url(#path-1-outside-1_1762_2056)"
+        d="M331.492 1L332.439 1.32234L332.889 0H331.492V1ZM30.2589 1V0H29.5887L29.334 0.619892L30.2589 1ZM1.49219 71L0.567245 70.6199L8.54731e-05 72H1.49219V71ZM312.543 71V72H313.36L313.523 71.1998L312.543 71ZM331.492 1V0H30.2589V1V2H331.492V1ZM30.2589 1L29.334 0.619892L0.567245 70.6199L1.49219 71L2.41713 71.3801L31.1839 1.38011L30.2589 1ZM312.543 71L313.523 71.1998C318.412 47.2245 324.754 23.8893 332.439 1.32234L331.492 1L330.546 0.67766C322.834 23.3249 316.469 46.7421 311.563 70.8002L312.543 71ZM1.49219 71V72H312.543V71V70H1.49219V71Z"
+        data-figma-gradient-fill="{&#34;type&#34;:&#34;GRADIENT_ANGULAR&#34;,&#34;stops&#34;:[{&#34;color&#34;:{&#34;r&#34;:0.20000000298023224,&#34;g&#34;:0.90588235855102539,&#34;b&#34;:1.0,&#34;a&#34;:1.0},&#34;position&#34;:0.0},{&#34;color&#34;:{&#34;r&#34;:0.0,&#34;g&#34;:0.0,&#34;b&#34;:0.0,&#34;a&#34;:1.0},&#34;position&#34;:0.50024455785751343}],&#34;stopsVar&#34;:[{&#34;color&#34;:{&#34;r&#34;:0.20000000298023224,&#34;g&#34;:0.90588235855102539,&#34;b&#34;:1.0,&#34;a&#34;:1.0},&#34;position&#34;:0.0},{&#34;color&#34;:{&#34;r&#34;:0.0,&#34;g&#34;:0.0,&#34;b&#34;:0.0,&#34;a&#34;:1.0},&#34;position&#34;:0.50024455785751343}],&#34;transform&#34;:{&#34;m00&#34;:5.3940565122173600e-13,&#34;m01&#34;:-956.025878906250,&#34;m02&#34;:1249.5588378906250,&#34;m10&#34;:575.31481933593750,&#34;m11&#34;:5.1916419297262650e-13,&#34;m12&#34;:-333.80203247070312},&#34;opacity&#34;:1.0,&#34;blendMode&#34;:&#34;NORMAL&#34;,&#34;visible&#34;:true}"
+        mask="url(#path-1-outside-1_649_278)"
       />
       <defs>
-        <clipPath id="paint0_angular_1762_2056_clip_path">
+        <clipPath id="paint0_angular_649_278_clip_path">
           <path
-            d="M331.701 1L332.627 1.37743L333.189 0H331.701V1ZM30.3814 1V0H29.7589L29.4841 0.558625L30.3814 1ZM1.60645 59.5L0.709123 59.0586L0.00013864 60.5H1.60645V59.5ZM312.747 59.5V60.5H313.532L313.718 59.7371L312.747 59.5ZM331.701 1V0H30.3814V1V2H331.701V1ZM30.3814 1L29.4841 0.558625L0.709123 59.0586L1.60645 59.5L2.50377 59.9414L31.2788 1.44138L30.3814 1ZM312.747 59.5L313.718 59.7371C318.605 39.7144 324.945 20.2255 332.627 1.37743L331.701 1L330.775 0.622573C323.057 19.5607 316.686 39.1434 311.775 59.2629L312.747 59.5ZM1.60645 59.5V60.5H312.747V59.5V58.5H1.60645V59.5Z"
-            mask="url(#path-1-outside-1_1762_2056)"
+            d="M331.492 1L332.439 1.32234L332.889 0H331.492V1ZM30.2589 1V0H29.5887L29.334 0.619892L30.2589 1ZM1.49219 71L0.567245 70.6199L8.54731e-05 72H1.49219V71ZM312.543 71V72H313.36L313.523 71.1998L312.543 71ZM331.492 1V0H30.2589V1V2H331.492V1ZM30.2589 1L29.334 0.619892L0.567245 70.6199L1.49219 71L2.41713 71.3801L31.1839 1.38011L30.2589 1ZM312.543 71L313.523 71.1998C318.412 47.2245 324.754 23.8893 332.439 1.32234L331.492 1L330.546 0.67766C322.834 23.3249 316.469 46.7421 311.563 70.8002L312.543 71ZM1.49219 71V72H312.543V71V70H1.49219V71Z"
+            mask="url(#path-1-outside-1_649_278)"
           />
         </clipPath>
       </defs>
@@ -72,19 +73,185 @@ function SvgEl() {
   );
 }
 
+const whiteBars = [
+  { top: "6%", num: 360 },
+  { top: "24%", num: 320 },
+  { top: "42%", num: 278 },
+  { top: "58%", num: 118 },
+  { top: "76%", num: 77 },
+  { top: "92%", num: 36 },
+];
+
+const nameTOBar: Array<{ name: string; bar: number }> = [
+  { name: "developers", bar: 4 },
+  { name: "sponsors", bar: 12 },
+  { name: "media partners", bar: 20 },
+  { name: "state", bar: 28 },
+  { name: "events", bar: 36 },
+  { name: "contact us", bar: 45 },
+];
+
 export default function Ham() {
+  const sideBarRef = useRef<HTMLDivElement>(null);
+  const mainSpeedRef = useRef<HTMLDivElement>(null);
+  const luvRef = useRef<HTMLDivElement>(null);
+  const barsRef = useRef<HTMLDivElement[]>([]);
+  const nameBoxesRef = useRef<HTMLDivElement[]>([]);
+  const [barsAnimated, setbarsAnimated] = useState(0);
+  const [entryAnim, setentryAnim] = useState(false);
+
+  const onClickNameBars = (num: number) => {
+    if (num > barsAnimated) {
+      gsap
+        .timeline({
+          ease: "power2.out",
+          onComplete: () => setbarsAnimated(num),
+        })
+        .to(barsRef.current.slice(barsAnimated, num), {
+          backgroundColor: "#b301ff",
+          boxShadow: "0px 0px 15px 0.5px #b301ff",
+          stagger: 0.02,
+        });
+      gsap.to(sideBarRef.current, {
+        backgroundPosition: `0% ${num * (100 / 48)}%`,
+        duration: 0.02 * (num - barsAnimated),
+      });
+    } else {
+      gsap
+        .timeline({
+          ease: "power2.out",
+          onComplete: () => setbarsAnimated(num),
+        })
+        .to(barsRef.current.slice(num, barsAnimated), {
+          backgroundColor: "#252727",
+          boxShadow: "none",
+          stagger: {
+            each: 0.02,
+            from: "end",
+          },
+        });
+      gsap.to(sideBarRef.current, {
+        backgroundPosition: `0% ${num * (100 / 48)}%`,
+        duration: 0.02 * (barsAnimated - num),
+      });
+    }
+  };
+
+  useEffect(() => {
+    const animBox = (ind: number) => {
+      gsap.timeline({ ease: "power2.out" }).to(nameBoxesRef.current[ind], {
+        transform: "translateX(0%)",
+        opacity: 1,
+        duration: 0.3,
+      });
+    };
+    const mainAnim = () => {
+      gsap
+        .timeline({
+          ease: "power2.out",
+          onComplete: () => {
+            (setbarsAnimated(48), setentryAnim(true));
+          },
+        })
+        .to(barsRef.current, {
+          backgroundColor: "#b301ff",
+          boxShadow: "0px 0px 15px 0.5px #b301ff",
+          stagger: {
+            each: 0.02,
+            onStart: function (this: gsap.core.Tween) {
+              const el = this.targets()[0] as HTMLDivElement;
+              const index = barsRef.current.indexOf(el);
+              if (index === 7) animBox(0);
+              else if (index === 15) animBox(1);
+              else if (index === 23) animBox(2);
+              else if (index === 31) animBox(3);
+              else if (index === 39) animBox(4);
+              else if (index === 47) animBox(5);
+            },
+          },
+        });
+      gsap.to(sideBarRef.current, {
+        backgroundPosition: `0% 100%`,
+        duration: 1.4,
+      });
+    };
+
+    gsap
+      .timeline({
+        defaults: { ease: "power1.inOut" },
+        onComplete: mainAnim,
+      })
+      .fromTo(
+        mainSpeedRef.current,
+        { opacity: 0, scale: 0, x: -200 },
+        {
+          opacity: 1,
+          scale: 1,
+          x: 0,
+          duration: 0.3,
+        },
+      )
+      .fromTo(
+        luvRef.current,
+        { scale: 0, opacity: 0, y: -20 },
+        { scale: 1, opacity: 1, y: 0, duration: 0.2 },
+      );
+  }, []);
+
   return (
     <div className={styles.container}>
+      <div className={styles.scanlineoverlay}></div>
       <img src={bg} alt="Ham" className={styles.bgImg} />
-      <div className={styles.mainBox}>
-        {Array.from({ length: 6 }).map((_, i) => (
-          <div className={styles.box} key={i}>
-            <SvgEl />
+      <div className={styles.innerCont} ref={mainSpeedRef}>
+        <div className={styles.mainBox}>
+          {nameTOBar.map((item, i) => (
+            <div
+              className={styles.box}
+              key={i}
+              ref={(el) => {
+                if (el) nameBoxesRef.current[i] = el;
+              }}
+              onClick={() => {
+                if (entryAnim) onClickNameBars(item.bar);
+              }}
+            >
+              <SvgEl />
+              <span>{item.name}</span>
+              <img src={textBottom} alt="textBottom" />
+            </div>
+          ))}
+        </div>
+        <div className={styles.sideBox}>
+          <div className={styles.lowerStyles}>
+            {Array.from({ length: 48 }).map((_, i) => (
+              <div
+                className={styles.bar}
+                key={i}
+                ref={(el) => {
+                  if (el) barsRef.current[i] = el;
+                }}
+              ></div>
+            ))}
+            {whiteBars.map((bar, i) => (
+              <div className={styles.whitebar} key={i} style={{ top: bar.top }}>
+                <span>{bar.num}</span>
+              </div>
+            ))}
+            <div className={styles.sideBar} ref={sideBarRef}></div>
+            <div className={styles.sideBarGrey}></div>
           </div>
-        ))}
+          <div className={styles.upperStyles}>
+            <div className={styles.barsContainer}>
+              {Array.from({ length: 32 }).map((_, i) => (
+                <div className={styles.bar} key={i}></div>
+              ))}
+            </div>
+            <div className={styles.angleBar}></div>
+          </div>
+        </div>
       </div>
 
-      <div className={styles.luvBar}>
+      <div className={styles.luvBar} ref={luvRef}>
         Made with <img src={luv} alt="love" /> by DVM
       </div>
     </div>
