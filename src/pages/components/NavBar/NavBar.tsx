84dec44e1a99827ccc195df88ff621ad524b31eb
Pranjal Gupta <pranjalgupta58.97@gmail.com>
2026-03-28 21:57:44 +0530

    navbar + mobileham

--- src/pages/components/NavBar/NavBar.tsx
+++ src/pages/components/NavBar/NavBar.tsx
@@ -1,59 +1,157 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./NavBar.module.scss";
-import { FaInstagram, } from "react-icons/fa";
-import { FaLinkedin, FaXTwitter } from "react-icons/fa6";
 import { useModalStore } from "../../../utils/store";
+import navBG from "/img/landing/navBG.png";
 
-function NavLink({ to, label }: { to: string, label: string }) {
+function NavLink({ to, label }: { to: string; label: string }) {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  return (
+    <a className={styles.navLink} onClick={() => navigate(to)}>
+      {/* <svg
+        width="232"
+        height="54"
+        viewBox="0 0 232 54"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M27.618 12.8773V2.50498C27.618 2.06102 27.0825 1.83727 26.7666 2.14925L2.14864 26.4647C2.05353 26.5587 2 26.6868 2 26.8205V50.8083C2 51.0845 2.22386 51.3083 2.5 51.3083H205.022C205.159 51.3083 205.289 51.2527 205.384 51.1542L229.861 25.5913C229.95 25.4982 230 25.3744 230 25.2455V13.8773C230 13.6012 229.776 13.3773 229.5 13.3773H28.118C27.8418 13.3773 27.618 13.1535 27.618 12.8773Z"
+          stroke="#18B0A1"
+          stroke-width="3"
+        />
+      </svg> */}
+      {/* <svg
+        width="231"
+        height="53"
+        viewBox="0 0 231 53"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <foreignObject x="-10" y="-10" width="251" height="72.3074">
+          <div
+            // xmlns="http://www.w3.org/1999/xhtml"
+            style={{
+              backdropFilter: "blur(5px)",
+              clipPath: "url(#bgblur_0_676_239_clip_path)",
+              height: "100%",
+              width: "100%",
+            }}
+          ></div>
+        </foreignObject>
+        <path
+          data-figma-bg-blur-radius="10"
+          d="M27.118 12.3763V2.00401C27.118 1.56004 26.5825 1.33629 26.2666 1.64827L1.64864 25.9637C1.55353 26.0577 1.5 26.1858 1.5 26.3195V50.3074C1.5 50.5835 1.72386 50.8074 2 50.8074H204.522C204.659 50.8074 204.789 50.7517 204.884 50.6532L229.361 25.0903C229.45 24.9973 229.5 24.8734 229.5 24.7445V13.3763C229.5 13.1002 229.276 12.8763 229 12.8763H27.618C27.3418 12.8763 27.118 12.6525 27.118 12.3763Z"
+          stroke="#18B0A1"
+          stroke-width="3"
+        />
+        <defs>
+          <clipPath
+            id="bgblur_0_676_239_clip_path"
+            transform="translate(10 10)"
+          >
+            <path d="M27.118 12.3763V2.00401C27.118 1.56004 26.5825 1.33629 26.2666 1.64827L1.64864 25.9637C1.55353 26.0577 1.5 26.1858 1.5 26.3195V50.3074C1.5 50.5835 1.72386 50.8074 2 50.8074H204.522C204.659 50.8074 204.789 50.7517 204.884 50.6532L229.361 25.0903C229.45 24.9973 229.5 24.8734 229.5 24.7445V13.3763C229.5 13.1002 229.276 12.8763 229 12.8763H27.618C27.3418 12.8763 27.118 12.6525 27.118 12.3763Z" />
+          </clipPath>
+        </defs>
+      </svg> */}
+      <svg
+        width="251"
+        height="73"
+        viewBox="0 0 251 73"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <foreignObject x="-2" y="-6" width="255" height="78.3074">
+          <div
+            style={{
+              backdropFilter: "blur(50px)",
+              clipPath: "url(#bgblur_0_676_239_clip_path)",
+              height: "100%",
+              width: "100%",
+            }}
+          ></div>
+        </foreignObject>
+        <g filter="url(#filter0_d_676_239)" data-figma-bg-blur-radius="12">
+          <path
+            d="M37.118 18.3763V8.00401C37.118 7.56004 36.5825 7.33629 36.2666 7.64827L11.6486 31.9637C11.5535 32.0577 11.5 32.1858 11.5 32.3195V56.3074C11.5 56.5835 11.7239 56.8074 12 56.8074H214.522C214.659 56.8074 214.789 56.7517 214.884 56.6532L239.361 31.0903C239.45 30.9973 239.5 30.8734 239.5 30.7445V19.3763C239.5 19.1002 239.276 18.8763 239 18.8763H37.618C37.3418 18.8763 37.118 18.6525 37.118 18.3763Z"
+            fill="#18B0A1"
+            fill-opacity="0.2"
+            shape-rendering="crispEdges"
+          />
+          <path
+            d="M37.118 18.3763V8.00401C37.118 7.56004 36.5825 7.33629 36.2666 7.64827L11.6486 31.9637C11.5535 32.0577 11.5 32.1858 11.5 32.3195V56.3074C11.5 56.5835 11.7239 56.8074 12 56.8074H214.522C214.659 56.8074 214.789 56.7517 214.884 56.6532L239.361 31.0903C239.45 30.9973 239.5 30.8734 239.5 30.7445V19.3763C239.5 19.1002 239.276 18.8763 239 18.8763H37.618C37.3418 18.8763 37.118 18.6525 37.118 18.3763Z"
+            stroke="#18B0A1"
+            stroke-width="3"
+            shape-rendering="crispEdges"
+          />
+        </g>
+        <defs>
+          <filter
+            id="filter0_d_676_239"
+            x="-2"
+            y="-6"
+            width="255"
+            height="78.3074"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dy="4" />
+            <feGaussianBlur stdDeviation="5" />
+            <feComposite in2="hardAlpha" operator="out" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_676_239"
+            />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="effect1_dropShadow_676_239"
+              result="shape"
+            />
+          </filter>
+          <clipPath id="bgblur_0_676_239_clip_path" transform="translate(2 6)">
+            <path d="M37.118 18.3763V8.00401C37.118 7.56004 36.5825 7.33629 36.2666 7.64827L11.6486 31.9637C11.5535 32.0577 11.5 32.1858 11.5 32.3195V56.3074C11.5 56.5835 11.7239 56.8074 12 56.8074H214.522C214.659 56.8074 214.789 56.7517 214.884 56.6532L239.361 31.0903C239.45 30.9973 239.5 30.8734 239.5 30.7445V19.3763C239.5 19.1002 239.276 18.8763 239 18.8763H37.618C37.3418 18.8763 37.118 18.6525 37.118 18.3763Z" />
+          </clipPath>
+        </defs>
+      </svg>
 
-    return (
-        <a className={styles.navLink} onClick={() => navigate(to)}>
-            {
-                ["[", ...label.split(""), "]"].map((char, index) => (
-                    <span key={index} className={styles.navLinkChar}>{char}</span>
-                ))
-            }
-        </a>
-    )
+      {label}
+    </a>
+  );
 }
 
 export default function NavBar() {
-
-    // const location = useLocation();
-    const isModalOpen = useModalStore((s) => s.isModalOpen);
-    return (
-        <div className={styles.navbarContainer}>
-            <div className={`${styles.navbar} ${isModalOpen ? styles.hiddenNavbar : ""}`}>
-                <div className={styles.titleLogo}>
-                    <img className={styles.logo} src="/img/apogee26.png" alt="Logo" />
-                </div>
-                <div className={styles.navlinks}>
-                    <NavLink to="/city" label="Home" />
-                    <NavLink to="/about" label="About" />
-                    <NavLink to="/events" label="Events" />
-                    <NavLink to="/contact" label="Contact   Us" />
-                    <NavLink to="/speakers" label="Speakers" />
-                </div>
-                <div className={styles.socialLinks}>
-                    <div className={styles.socialLinkContainer}>
-                        <a className={styles.socialLink} >
-                            <FaInstagram className={styles.socialIcon} />
-                        </a>
-                    </div>
-                    <div className={styles.socialLinkContainer}>
-                        <a className={styles.socialLink} >
-                            <FaLinkedin className={styles.socialIcon} />
-                        </a>
-                    </div>
-                    <div className={styles.socialLinkContainer}>
-                        <a className={styles.socialLink} >
-                            <FaXTwitter className={styles.socialIcon} />
-                        </a>
-                    </div>
-                </div>
-            </div>
+  const isModalOpen = useModalStore((s) => s.isModalOpen);
+  return (
+    <div className={styles.navbarContainer}>
+      <img
+        src={navBG}
+        alt="Nav Background"
+        className={`${styles.navBG}  ${isModalOpen ? styles.hiddenNavBG : ""}`}
+      />
+      <div
+        className={`${styles.navbar} ${isModalOpen ? styles.hiddenNavbar : ""}`}
+      >
+        <div className={styles.navlinks}>
+          <NavLink to="/" label="Home" />
+          <NavLink to="/about" label="About" />
+          <NavLink to="/events" label="Events" />
+          <NavLink to="/contact" label="Contact   Us" />
+          <NavLink to="/speakers" label="Speakers" />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/pages/ham/HamMobile.tsx
+++ src/pages/ham/HamMobile.tsx
@@ -82,13 +82,13 @@ const whiteBars = [
   { top: "92%", num: 36 },
 ];
 
-const nameTOBar: Array<{ name: string; bar: number }> = [
-  { name: "developers", bar: 4 },
-  { name: "sponsors", bar: 12 },
-  { name: "media partners", bar: 20 },
-  { name: "state", bar: 28 },
-  { name: "events", bar: 36 },
-  { name: "contact us", bar: 45 },
+const nameTOBar: Array<{ name: string; bar: number; url: string }> = [
+  { name: "developers", bar: 4, url: "/developers" },
+  { name: "sponsors", bar: 12, url: "/sponsors" },
+  { name: "media partners", bar: 20, url: "/media-partners" },
+  { name: "getting to pilani", bar: 28, url: "/getting-to-pilani" },
+  { name: "events", bar: 36, url: "/events" },
+  { name: "contact us", bar: 45, url: "/contact-us" },
 ];
 
 export default function Ham() {
@@ -100,12 +100,15 @@ export default function Ham() {
   const [barsAnimated, setbarsAnimated] = useState(0);
   const [entryAnim, setentryAnim] = useState(false);
 
-  const onClickNameBars = (num: number) => {
+  const onClickNameBars = (item: (typeof nameTOBar)[0]) => {
+    const { bar: num, url } = item;
     if (num > barsAnimated) {
       gsap
         .timeline({
           ease: "power2.out",
-          onComplete: () => setbarsAnimated(num),
+          onComplete: () => {
+            (setbarsAnimated(num), (window.location.href = url));
+          },
         })
         .to(barsRef.current.slice(barsAnimated, num), {
           backgroundColor: "#b301ff",
@@ -120,7 +123,10 @@ export default function Ham() {
       gsap
         .timeline({
           ease: "power2.out",
-          onComplete: () => setbarsAnimated(num),
+          onComplete: () => {
+            setbarsAnimated(num);
+            // window.location.href = url;
+          },
         })
         .to(barsRef.current.slice(num, barsAnimated), {
           backgroundColor: "#252727",
@@ -212,7 +218,7 @@ export default function Ham() {
                 if (el) nameBoxesRef.current[i] = el;
               }}
               onClick={() => {
-                if (entryAnim) onClickNameBars(item.bar);
+                if (entryAnim) onClickNameBars(item);
               }}
             >
               <SvgEl />
@@ -241,11 +247,20 @@ export default function Ham() {
             <div className={styles.sideBarGrey}></div>
           </div>
           <div className={styles.upperStyles}>
-            <div className={styles.barsContainer}>
-              {Array.from({ length: 32 }).map((_, i) => (
-                <div className={styles.bar} key={i}></div>
-              ))}
-            </div>
+            <svg
+              width="25"
+              height="34"
+              viewBox="0 0 25 34"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className={styles.svgBars}
+            >
+              <path
+                d="M1.40039 33.4971L1.33105 33.5L1.26562 33.502L1.2373 33.4424L0.966797 32.8662L1.40039 33.4971ZM3.05078 33.4424L2.58691 33.458L0 29.6934L0.213867 29.3164L3.05078 33.4424ZM4.7002 33.3877L4.2373 33.4033L0.762695 28.3496L0.976562 27.9717L4.7002 33.3877ZM6.35059 33.334L5.8877 33.3486L1.52637 27.0059L1.74023 26.6279L6.35059 33.334ZM8.00098 33.2793L7.53711 33.2939L2.29004 25.6611L2.50391 25.2842L8.00098 33.2793ZM9.65039 33.2246L9.1875 33.2402L3.05273 24.3174L3.26758 23.9404L9.65039 33.2246ZM10.8135 32.4609L10.6299 32.8838L3.81641 22.9736L4.03125 22.5957L10.8135 32.4609ZM11.4658 30.9551L11.2822 31.3779L4.58008 21.6299L4.79492 21.252L11.4658 30.9551ZM12.1182 29.4492L11.9346 29.8721L5.34375 20.2852L5.55859 19.9082L12.1182 29.4492ZM12.7705 27.9443L12.5869 28.3672L6.10742 18.9414L6.32129 18.5645L12.7705 27.9443ZM13.4229 26.4385L13.2393 26.8613L6.87109 17.5977L7.08496 17.2207L13.4229 26.4385ZM14.0752 24.9326L13.8916 25.3555L7.63477 16.2539L7.84863 15.876L14.0752 24.9326ZM14.7275 23.4277L14.5449 23.8506L8.39746 14.9102L8.6123 14.5322L14.7275 23.4277ZM15.3799 21.9219L15.1973 22.3447L9.16113 13.5654L9.37598 13.1885L15.3799 21.9219ZM16.0322 20.416L15.8496 20.8389L9.9248 12.2217L10.1396 11.8447L16.0322 20.416ZM16.6846 18.9111L16.502 19.334L10.6885 10.8779L10.9033 10.5L16.6846 18.9111ZM17.3379 17.4053L17.1543 17.8281L11.4521 9.53418L11.666 9.15625L17.3379 17.4053ZM17.9902 15.8994L17.8066 16.3223L12.2158 8.19043L12.4297 7.8125L17.9902 15.8994ZM18.6426 14.3945L18.459 14.8174L12.9795 6.8457L13.1934 6.46875L18.6426 14.3945ZM19.2949 12.8887L19.1113 13.3115L13.7422 5.50195L13.957 5.125L19.2949 12.8887ZM19.9473 11.3828L19.7637 11.8057L14.5059 4.1582L14.7207 3.78027L19.9473 11.3828ZM20.5996 9.87793L20.416 10.3008L15.2695 2.81445L15.4844 2.43652L20.5996 9.87793ZM21.252 8.37207L21.0693 8.79492L16.0332 1.46973L16.2471 1.09277L21.252 8.37207ZM21.9043 6.86621L21.7217 7.28906L16.7969 0.125977L16.8408 0.0498047L16.8701 0H17.1836L21.9043 6.86621ZM22.5566 5.36133L22.374 5.78418L18.3975 0H18.8711L22.5566 5.36133ZM23.209 3.85547L23.0264 4.27832L20.085 0H20.5586L23.209 3.85547ZM23.8623 2.34961L23.6787 2.77246L21.7725 0H22.2461L23.8623 2.34961ZM24.5146 0.844727L24.3311 1.26758L23.46 0H23.9336L24.5146 0.844727Z"
+                fill="#252727"
+              />
+            </svg>
+
             <div className={styles.angleBar}></div>
           </div>
         </div>
