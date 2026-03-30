f5c48b680cecb66f44c4fe4aa4545e6e9fcc3555
AnonymousTechDeveloper <anshmalviya54@gmail.com>
2026-03-30 23:13:09 +0530

    Updated animation

--- src/pages/city/components/ScrollSync/ScrollSync.tsx
+++ src/pages/city/components/ScrollSync/ScrollSync.tsx
@@ -2,7 +2,7 @@ import { useScroll } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { getProject } from "@theatre/core";
 
-import state from "../../state-stray.json";
+import state from "../../state-walk.json";
 import { useActiveSheetStore, useModalStore, useNavStateStore, usePreloaderStateStore, useScrollStore } from "../../../../utils/store";
 import { type Section, useCurrentSectionStore } from "../../../../utils/store";
 import { useEffect, useRef } from "react";
