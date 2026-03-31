a8f89f9bd823e36cf0c078c2068901c3fa0637be
AnonymousTechDeveloper <anshmalviya54@gmail.com>
2026-03-31 14:33:00 +0530

    Kill pushProgress train fixed

--- src/pages/city/components/Modal/DirectionlUnlock.tsx
+++ src/pages/city/components/Modal/DirectionlUnlock.tsx
@@ -69,6 +69,7 @@ export default function DirectionalUnlock({ containerRef, modalUIRef }: Directio
             },
             onStop: () => {
                 if (pullProgress > 0 && pullProgress < THRESHOLD) {
+                    decayTimer.current && clearTimeout(decayTimer.current);
                     decayTimer.current = setTimeout(() => {
                         snapAnim.current = gsap.to({ val: pullProgress }, {
                             val: 0,
@@ -116,6 +117,7 @@ export default function DirectionalUnlock({ containerRef, modalUIRef }: Directio
 
         const handleTouchEnd = () => {
             if (pullProgress > 0 && pullProgress < THRESHOLD) {
+                decayTimer.current && clearTimeout(decayTimer.current);
                 decayTimer.current = setTimeout(() => {
                     snapAnim.current = gsap.to({ val: pullProgress }, {
                         val: 0,
