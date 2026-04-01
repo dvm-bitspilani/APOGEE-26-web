6cf8f9246f29b53e3933cd833d2d9ef82e58ba5e
Ishaan02-Coder <ishaandaftari2@gmail.com>
2026-04-01 21:58:42 +0530

    Cleaning code

--- src/pages/city/components/models/Logo.tsx
+++ src/pages/city/components/models/Logo.tsx
@@ -1,67 +1,47 @@
 import * as THREE from 'three'
-import  { useRef, type JSX } from 'react'
+import { useRef, type JSX } from 'react'
 import { Center, useGLTF } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
-import type { GLTF } from 'three-stdlib'
 import { useHolographicMaterial } from '../../hooks/useHolographicMaterial'
-// import { materialAlphaTest } from 'three/src/nodes/TSL.js'
-// import { useCyberpunkFogMaterial } from '../../hooks/useCyberPunkFogMaterial'
-
-type GLTFResult = GLTF & {
-  nodes: {
-    Plane001: THREE.Mesh
-    Plane002: THREE.Mesh
-  }
-  materials: {
-    ['Material.002']: THREE.MeshStandardMaterial
-    ['Material.003']: THREE.MeshStandardMaterial
-  }
-}
+import type { GLTFLogo } from '../../types/Models.types'
 
 export function Logo(props: JSX.IntrinsicElements['group']) {
-  const { nodes } = useGLTF('/models/dvmlogowide-v1.glb') as unknown as GLTFResult
-  // const mat = useCyberpunkFogMaterial()
-  const mat = useHolographicMaterial([0, 0.8, 0.9], 1,2.0);
-
-  // 🔥 separate refs
-  const mesh1Ref = useRef<THREE.Mesh>(null!)
-  const mesh2Ref = useRef<THREE.Mesh>(null!)
-
-  // 🔄 animate rotation on Y axis
- const groupRef = useRef<THREE.Group>(null!)
- const group1Ref = useRef<THREE.Group>(null!)
-const group2Ref = useRef<THREE.Group>(null!)
-
-useFrame((_, delta) => {
-  groupRef.current.rotation.y += delta
-  // group2Ref.current.rotation.y -= delta
-})
+  const { nodes } = useGLTF('/models/dvmlogowide-v1.glb') as unknown as GLTFLogo
+  const mat = useHolographicMaterial([0, 0.8, 0.9], 1, 2.0);
+  // const mesh1Ref = useRef<THREE.Mesh>(null!)
+  // const mesh2Ref = useRef<THREE.Mesh>(null!)
+  const groupRef = useRef<THREE.Group>(null!)
+  // const group1Ref = useRef<THREE.Group>(null!)
+  // const group2Ref = useRef<THREE.Group>(null!)
+  useFrame((_, delta) => {
+    groupRef.current.rotation.y += delta
+  })
   return (
-    <group {...props} dispose={null} rotation={[0, Math.PI, 0]}ref={groupRef}>
+    <group {...props} dispose={null} rotation={[0, Math.PI, 0]} ref={groupRef}>
       <Center>
-      <group ref={group1Ref}>
-  <mesh
-    ref={mesh1Ref}
-    geometry={nodes.Plane001.geometry}
-    material={mat}
-    position={[-0.35, -0.125, -0.56]}
-    rotation={[Math.PI / 2, 0, 0]}
-    scale={[0.064,0.064*2,0.064]}
-  />
-</group>
+        <group >
+          <mesh
+            // ref={mesh1Ref}
+            geometry={nodes.Plane001.geometry}
+            material={mat}
+            position={[-0.35, -0.125, -0.56]}
+            rotation={[Math.PI / 2, 0, 0]}
+            scale={[0.064, 0.064 * 2, 0.064]}
+          />
+        </group>
 
-<group ref={group2Ref}>
-  <mesh
-    ref={mesh2Ref}
-    geometry={nodes.Plane002.geometry}
-    material={mat}
-    position={[0.616, 0.125, -0.56]}
-    rotation={[Math.PI / 2, 0, 0]}
-    scale={[0.088, 0.087*2, 0.087]}
-  />
+        <group >
+          <mesh
+            // ref={mesh2Ref}
+            geometry={nodes.Plane002.geometry}
+            material={mat}
+            position={[0.616, 0.125, -0.56]}
+            rotation={[Math.PI / 2, 0, 0]}
+            scale={[0.088, 0.087 * 2, 0.087]}
+          />
 
-</group>
-  </Center>
+        </group>
+      </Center>
     </group>
   )
 }
--- src/pages/city/components/models/StarJunstion3.tsx
+++ src/pages/city/components/models/StarJunstion3.tsx
@@ -1,13 +1,9 @@
 
 import { Clone, useGLTF } from '@react-three/drei'
 import { type GLTFResult } from '../../types/starJunction.types';
-// import { useEffect } from 'react';
-// import { CityModel } from './Citywithoutglobe';
-
 const BLOCKS = 4;
 const SPACING = 44.1;
 const positions: [number, number, number][] = Array(BLOCKS).fill(0).map((_, i) => [0, 0.01 * (i % 2), i * SPACING])
-
 export default function StarJunction() {
   // const { nodes, materials } = useGLTF('/models/city10feb-transformed.glb') as unknown as GLTFResult
   const { scene } = useGLTF('/models/city33-transformed.glb') as unknown as GLTFResult
@@ -25,13 +21,4 @@ export default function StarJunction() {
     ))
   )
 }
-// export default function StarJunction() {
-//   return (
-//     <>
-//       {positions.map((pos, i) => (
-//         <CityModel key={i} position={pos} scale={1.5} rotation={[0, Math.PI / 2, 0]}/>
-//       ))}
-//     </>
-//   )
-// }
 useGLTF.preload('/models/city33-transformed.glb')
